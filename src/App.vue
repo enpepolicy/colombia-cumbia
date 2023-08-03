71a548a73ad345e616bae312ea5e10aebbc4b6f9
enpepolicy <45740438+enpepolicy@users.noreply.github.com>
2023-08-03 23:45:21 +0200

    test: build

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Countdown from './components/Countdown.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Countdown class="text-4xl lg:text-7xl w-full h-full" :deadline="'2023-10-03T13:00:00'"/>
 </template>
 
 <style scoped>
